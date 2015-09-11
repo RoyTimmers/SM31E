f3668c45389e9005496cad726b463b09b82866ff
RoyTimmers <fhict@MacBook-Pro.local>
2015-09-11 10:23:52 +0200

    Practicum 2

---
+++ Any App/Any App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Any App
+//
+//  Created by Fhict on 11/09/15.
+//  Copyright (c) 2015 Roy Timmers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
